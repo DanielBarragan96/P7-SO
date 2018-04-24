d117273ccff549ec2bf2cdfc01fc6a778adf5103
santiago <31044042+SantSou@users.noreply.github.com>
2018-04-24 16:43:48 -0500

    moved comparations to external function

--- pagefaultVM.c
+++ pagefaultVM.c
@@ -18,6 +18,20 @@ int getfreeframe();
 
 int frames_used = 0;
 
+int getPag_a_expulsar(int* frames){
+	int pag_exp = 0;
+	if(ptbr[frames[0]].tlastaccess < ptbr[frames[1]].tlastaccess && ptbr[frames[0]].tlastaccess < ptbr[frames[2]].tlastaccess){
+            pag_exp = frames[0];//the frame 0 is the one to move to virtual memory
+        }
+        else if(ptbr[frames[1]].tlastaccess < ptbr[frames[0]].tlastaccess && ptbr[frames[1]].tlastaccess < ptbr[frames[2]].tlastaccess){
+            pag_exp = frames[1];//the frame 1 is the one to move to virtual memory
+        }
+        else{
+            pag_exp = frames[2];//the frame 2 is the one to move to virtual memory
+	}
+	return pag_exp;		
+}
+
 // Rutina de fallos de página
 
 int pagefault(char *vaddress)
@@ -44,20 +58,11 @@ int pagefault(char *vaddress)
             if(ptbr[j].presente == 1 && 3>f)
                 frames[f++] = j;
 
-//////////////////////////////checar cual de los access fue el menos reciente
-        if(ptbr[frames[0]].tlastaccess < ptbr[frames[1]].tlastaccess && ptbr[frames[0]].tlastaccess < ptbr[frames[2]].tlastaccess){
-            pag_a_expulsar = frames[0];//the frame 0 is the one to move to virtual memory
-        }
-        else if(ptbr[frames[1]].tlastaccess < ptbr[frames[0]].tlastaccess && ptbr[frames[1]].tlastaccess < ptbr[frames[2]].tlastaccess){
-            pag_a_expulsar = frames[1];//the frame 1 is the one to move to virtual memory
-        }
-        else{
-            pag_a_expulsar = frames[2];//the frame 2 is the one to move to virtual memory
-	}		
+////////checar cual de los access fue el menos reciente
+       pag_a_expulsar = getPag_a_expulsar(&frames[0]);
 
 		//avisa al sistema que la pagina ahora está libre ("no" ha sido modificada)
-            ptbr[pag_a_expulsar].modificado = 0;
-
+       ptbr[pag_a_expulsar].modificado = 0;
 		//avisar al sistema que la pagina no esta ocupando memoria fisica
        ptbr[pag_a_expulsar].presente = 0;
 		//guardar la dirección de la página a liberar
